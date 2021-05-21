93bf314659b3917d14f687e860cc79d122a4b616
Christian Sheen <csheen1725@gmail.com>
2021-05-21 12:56:34 -0400

    defined routes within server.js

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ connectDB();
 
 app.get("/", (req, res) => res.send("API Running"));
 
+//Define our routes
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/user", require("./routes/api/user"));
+app.use("/api/boats", require("./routes/api/boats"));
+app.use("/api/contactForm", require("./routes/api/contactForm"));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
