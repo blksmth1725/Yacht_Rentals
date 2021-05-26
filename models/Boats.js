b6c6312140ecc00d7ac36d99e9c4990f0dbd5e2a
Christian Sheen <csheen1725@gmail.com>
2021-05-26 13:49:22 -0400

    Modified mongoose models to work with api rputes more effectively

--- models/Boats.js
+++ models/Boats.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const BoatSchema = new mongoose.Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "user",
+  },
   make: {
     type: String,
     required: true,
--- models/ContactForm.js
+++ models/ContactForm.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const ContactFormSchema = new mongoose.Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "user",
+  },
   name: {
     type: String,
     required: true,
